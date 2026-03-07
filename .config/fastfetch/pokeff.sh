2b78eb270e39a2954cf6d6a9882c6e727965435a
MohDak112 <omohanad345@gmail.com>
2026-03-07 15:22:30 -0300

    Updated pokeff

--- .config/fastfetch/pokeff.sh
+++ .config/fastfetch/pokeff.sh
@@ -1,8 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-TMP="/tmp/poke_logo.txt"
+tmpfile=$(mktemp)
 
-pokeget random --hide-name> "$TMP"
-fastfetch --logo "$TMP"
+if [ $# -gt 0 ]; then
+  pokeget "$@" --hide-name >"$tmpfile"
+else
+  pokeget random --hide-name >"$tmpfile"
+fi
 
-rm "$TMP"
+if grep -qi "pokemon not found" "$tmpfile"; then
+  echo "Pokemon not found."
+  rm "$tmpfile"
+  exit 1
+fi
+
+fastfetch --logo-type file --logo "$tmpfile"
+
+rm "$tmpfile"
